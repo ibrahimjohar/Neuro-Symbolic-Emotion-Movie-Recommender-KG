b3a70422d48d71832abe49d70d2b88ee60f7e827
Ibrahim Johar Farooqi <34939623+ibrahimjohar@users.noreply.github.com>
2025-12-23 01:10:20 +0500

    normalized genres in kb.

--- notebooks/04_prepare_movie_kb.py
+++ notebooks/04_prepare_movie_kb.py
@@ -31,3 +31,56 @@ df_clean.to_csv("data/movies_kb_step1.csv", index=False)
 print(df_clean.head())
 print(f"total movies: {len(df_clean)}")
 print(f"missing years: {df_clean['year'].isna().sum()}")
+
+GENRE_MAPPING = {
+    "Sci-Fi": "SciFi",
+    "Film-Noir": "FilmNoir",
+    "Children": "Family",
+    "War": "War",
+    "Drama": "Drama",
+    "Comedy": "Comedy",
+    "Romance": "Romance",
+    "Thriller": "Thriller",
+    "Horror": "Horror",
+    "Action": "Action",
+    "Adventure": "Adventure",
+    "Fantasy": "Fantasy",
+    "Animation": "Animation",
+    "Documentary": "Documentary",
+    "Mystery": "Mystery",
+    "Crime": "Crime",
+    "Musical": "Musical",
+    "Western": "Western"
+}
+
+#normalize genres to ontology-aligned labels
+def normalize_genres(genres_str):
+    if pd.isna(genres_str):
+        return None
+    
+    raw_genres = genres_str.split("|")
+    mapped = []
+    
+    for g in raw_genres:
+        if g in GENRE_MAPPING:
+            mapped.append(GENRE_MAPPING[g])
+            
+    #removing duplicates while preserving order
+    mapped = list(dict.fromkeys(mapped))
+    
+    if not mapped:
+        return None
+    
+    return "|".join(mapped)
+
+
+df_clean["genres_normalized"] = df_clean["genres"].apply(normalize_genres)
+#drop movie with no mappable genres
+df_clean = df_clean.dropna(subset=["genres_normalized"])
+#keep final cols
+df_final = df_clean[["movie_id", "title", "year", "genres_normalized"]]
+#save final kb
+df_final.to_csv("data/movie_kb_final.csv", index=False)
+
+print(df_final.head())
+print(f"total movies after genre normalization: {len(df_final)}")
